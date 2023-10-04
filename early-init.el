75c69c49f41e0fed91779e0ef855290913bb43e8
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-10-04 10:39:38 +0200

    Initial commit with basic config

---
+++ early-init.el
@@ -0,0 +1,12 @@
+;; Disable Emacs's default package manager
+(setq package-enable-at-startup nil)
+
+;; Move backup and autosave files to
+;; their respective subdirectories
+(setq backup-directory-alist
+      `((".*" . ,"~/.emacs.d/backups")))
+(setq auto-save-file-name-transforms
+      `((".*" ,"~/.emacs.d/auto-saves" t)))
+;; Set the auto save timeout interval to a lower value
+(setq auto-save-timeout 10
+      auto-save-interval 150)
---
+++ init.el
@@ -0,0 +1,99 @@
+;; Install and bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Install use-package using straight.el
+(straight-use-package 'use-package)
+;; Configure use-package to use straight.el by default
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
+
+;; Set the default font
+(set-frame-font "FantasqueSansM Nerd Font Mono-11" nil t)
+
+;; Remove annoying GUI elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+	doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-monokai-pro t)
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Change the treemacs colors
+  (setq doom-themes-treemacs-theme "doom-atom")
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+;; Show org-mode bullets as UTF-8 charactersx
+(use-package org-bullets
+  :ensure t
+  :custom
+  (org-bullets-bullet-list '("▸" "■" "◈" "◉" "◊"))
+  :config
+  (custom-set-faces
+  '(org-level-1 ((t (:inherit outline-1 :height 2.0))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+  '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(use-package magit
+  :ensure t
+  :bind
+  ("C-x g" . magit-status)
+  ("C-x M-g" . magit-dispatch)
+  ("C-c M-g" . magit-file-dispatch))
+
+;; Configure org-mode and it's sub functions
+(use-package org
+  :straight (:type built-in)
+  :bind ("C-c a" . org-agenda)
+  :config
+  ;; Add my notes to the org-agenda
+  (defun org-agenda-files (&rest _)
+    (directory-files-recursively "~/docs/notes" org-agenda-file-regexp)))
+
+;; Build a second brain inside of
+;; Emacs with Org Roam
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/docs/notes/")
+  (org-roam-completion-everywhere t)
+  (org-roam-dailies-capture-templates
+   '(("d" "default" entry "* %<%I:%M %p>: %?"
+      :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)
+	 :map org-mode-map
+	 ("C-M-i" . completion-at-point)
+	 :map org-roam-dailies-map
+	 ("Y" . org-roam-dailies-capture-yesterday)
+	 ("T" . org-roam-dailies-capture-tomorrow))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-db-autosync-mode))
