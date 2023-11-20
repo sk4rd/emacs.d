cd6550237ea4bc93b40c5e325ca342d8a697b90c
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-20 22:00:25 +0100

    Improved config clarity

--- init.el
+++ init.el
@@ -20,37 +20,40 @@
   (straight-use-package-by-default t))
 (setq use-package-always-ensure t)
 
-;; Set the default font
-(setq default-frame-alist '((font . "Iosevka-12")))
+;; Check if Iosevka font is available and set it as the default font
+(when (member "Iosevka" (font-family-list))
+  (set-frame-font "Iosevka-12" nil t)
+  (setq default-frame-alist '((font . "Iosevka-12")))
+  (setq initial-frame-alist '((font . "Iosevka-12"))))
 
-;; Remove annoying GUI elements
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+;; Remove annoying GUI elements if they are available
+(when (fboundp 'menu-bar-mode)
+  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
 
 (use-package doom-themes
+  :custom
+  (doom-themes-enable-bold t)
+  (doom-themes-enable-italic t)
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-	doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-gruvbox t)
-  ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
-  ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
-;; Show org-mode bullets as UTF-8 charactersx
 (use-package org-bullets
   :custom
-  (org-bullets-bullet-list '("▸" "■" "◈" "◉" "◊"))
-  :config
-  (custom-set-faces
-   '(org-level-1 ((t (:inherit outline-1 :height 2.0))))
-   '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
-   '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
-   '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
-   '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  (org-bullets-bullet-list '("●" "◉" "◆" "◈" "▼")) ; Custom bullet points for org headings
+  :custom-face
+  (org-level-1 ((t (:inherit outline-1 :height 2.0)))) ; Larger font for top-level headings
+  (org-level-2 ((t (:inherit outline-2 :height 1.5)))) ; Medium font for second-level headings
+  (org-level-3 ((t (:inherit outline-3 :height 1.2)))) ; Slightly larger font for third-level headings
+  (org-level-4 ((t (:inherit outline-4 :height 1.0)))) ; Default font for fourth-level headings
+  (org-level-5 ((t (:inherit outline-5 :height 1.0)))) ; Default font for fifth-level headings
+  :hook
+  (org-mode . org-bullets-mode)) ; Enable org-bullets-mode automatically in org-mode
 
 (use-package magit
   :bind
@@ -58,68 +61,77 @@
   ("C-x M-g" . magit-dispatch)
   ("C-c M-g" . magit-file-dispatch))
 
-;; Configure org-mode and it's sub functions
+;; Configure org-mode and related features
 (use-package org
-  :straight (:type built-in)
   :bind ("C-c a" . org-agenda)
   :custom
   (org-attach-use-inheritance t)
   :config
-  ;; Add my notes to the org-agenda
-  (defun org-agenda-files (&rest _)
-    (directory-files-recursively "~/docs/notes" org-agenda-file-regexp)))
+  ;; Dynamically set org-agenda files from my notes directory
+  (setq org-agenda-files (directory-files-recursively "~/docs/notes" "\\.org$")))
 
-;; Install and configure org-roam
-;; for personal notes and school
+;; Configure org-roam for personal knowledge management
 (use-package org-roam
-  :after org
+  :after org  ; Load after org to ensure dependencies
   :init
-  (setq org-roam-v2-ack t)
+  (setq org-roam-v2-ack t)  ; Acknowledge migration to org-roam v2
   :custom
-  (org-roam-directory "~/docs/notes/")
-  (org-roam-completion-everywhere t)
+  (org-roam-directory "~/docs/notes/")  ; Set main directory for org-roam files
+  (org-roam-completion-everywhere t)  ; Enable org-roam completion across all org buffers
   (org-roam-dailies-capture-templates
-   '(("d" "default" entry "* %<%I:%M %p>: %?"
+   '(("d" "default" entry "* %<%I:%M %p>: %?"  ; Template for daily notes
       :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-	 ("C-c n f" . org-roam-node-find)
-	 ("C-c n i" . org-roam-node-insert)
-	 :map org-mode-map
-	 ("C-M-i" . completion-at-point)
-	 :map org-roam-dailies-map
-	 ("Y" . org-roam-dailies-capture-yesterday)
-	 ("T" . org-roam-dailies-capture-tomorrow))
+  :bind
+  (("C-c n l" . org-roam-buffer-toggle)  ; Toggle org-roam buffer
+   ("C-c n f" . org-roam-node-find)      ; Find org-roam node
+   ("C-c n i" . org-roam-node-insert)    ; Insert org-roam node
+   ("C-M-i" . completion-at-point)       ; Completion at point in org-mode
+   :map org-roam-dailies-map
+   ("Y" . org-roam-dailies-capture-yesterday)  ; Capture yesterday's daily note
+   ("T" . org-roam-dailies-capture-tomorrow))  ; Capture tomorrow's daily note
   :bind-keymap
-  ("C-c n d" . org-roam-dailies-map)
+  ("C-c n d" . org-roam-dailies-map)  ; Keymap for dailies
   :config
-  (require 'org-roam-dailies) ;; Ensure the keymap is available
-  (org-roam-db-autosync-mode))
+  (org-roam-setup)  ; Setup org-roam
+  (org-roam-db-autosync-mode))  ; Enable automatic syncing of the org-roam database
 
+;; Configure org-roam-ui for visualizing org-roam notes
 (use-package org-roam-ui
-  :straight
-    (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
-    :after org-roam
-    :config
-    (setq org-roam-ui-sync-theme t
-	  org-roam-ui-follow t
-	  org-roam-ui-update-on-save t
-	  org-roam-ui-open-on-start t))
+  :straight (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+  :after org-roam  ; Load after org-roam
+  :custom
+  (org-roam-ui-sync-theme t) ; Sync UI theme with Emacs
+  (org-roam-ui-follow t) ; Enable following the current node
+  (org-roam-ui-update-on-save t) ; Update UI graph on each save
+  (org-roam-ui-open-on-start t)) ; Open UI automatically at start
 
-(use-package all-the-icons)
+(use-package all-the-icons
+  :if (display-graphic-p)
+  :config
+  ;; Check if the fonts have been installed
+  (unless (file-exists-p "~/.emacs.d/.all-the-icons-installed")
+    ;; Temporarily override `yes-or-no-p` to always return t (yes)
+    (cl-letf (((symbol-function 'yes-or-no-p) (lambda (&rest args) t))
+              ((symbol-function 'y-or-n-p) (lambda (&rest args) t)))
+      ;; Install the fonts
+      (all-the-icons-install-fonts))
+    ;; Create a flag file to indicate the fonts have been installed
+    (with-temp-file "~/.emacs.d/.all-the-icons-installed" (insert "Done"))))
 
 (use-package all-the-icons-dired
   :hook (dired-mode . (lambda ()
-			(interactive)
-			(unless (file-remote-p default-directory)
-			  (all-the-icons-dired-mode)))))
+                        ;; Enable all-the-icons-dired-mode only for local directories
+                        (unless (file-remote-p default-directory)
+                          (all-the-icons-dired-mode)))))
 
 (use-package dired-subtree
   :config
+  ;; Refresh icons when toggling dired-subtree
   (advice-add 'dired-subtree-toggle :after (lambda ()
-					     (interactive)
 					     (when all-the-icons-dired-mode
 					       (revert-buffer))))
-  (bind-key "<tab>" #'dired-subtree-toggle dired-mode-map))
+  :bind (:map dired-mode-map
+         ("<tab>" . dired-subtree-toggle))) ; Bind <tab> to toggle subtrees in dired-mode
 
 (use-package which-key
   :config
