00e94c749aaff81f74b16c49f23f02d82385dcf4
Mikolaj Bajtkiewicz <mikolaj.bajtkiewicz@tutanota.de>
2023-11-30 09:27:39 +0100

    Added new template for org-roam

--- init.el
+++ init.el
@@ -112,6 +112,11 @@
    :custom
    (org-roam-directory "~/docs/notes/")
    (org-roam-completion-everywhere t)
+   (org-roam-capture-templates
+    '(("d" "default" plain
+       "%?"
+       :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+       :unnarrowed t)))
    (org-roam-dailies-capture-templates
     '(("d" "default" entry "* %<%I:%M %p>: %?"
        :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
@@ -126,7 +131,8 @@
    :bind-keymap
    ("C-c n d" . org-roam-dailies-map)
    :config
-   (require 'org-roam-dailies) ;; Ensure the keymap is available
+   (require 'org-roam-dailies) ;; Ensure the keymap is available     
+   (org-roam-setup)
    (org-roam-db-autosync-mode))
 
 ;; Configure org-roam-ui for visualizing org-roam notes
